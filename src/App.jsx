a91a6ac237c900ffd1220379159c62bd1db0f4e4
David Myung <djmyung07@gmail.com>
2026-06-01 21:59:05 -0400

    Add GitHub Pages deployment config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './pages/Home';
 import Search from './pages/Search';
